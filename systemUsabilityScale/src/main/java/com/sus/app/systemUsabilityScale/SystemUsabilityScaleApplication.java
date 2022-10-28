be2e5e2ae082e1be23c3375504dfef1c5f051e6d
simonhaughom <simon.haughom@gmail.com>
2022-10-28 13:42:13 +0200

    new

---
+++ systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/SystemUsabilityScaleApplication.java
@@ -0,0 +1,14 @@
+package com.sus.app.systemUsabilityScale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemUsabilityScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemUsabilityScaleApplication.class, args);
+
+		System.out.println("hello");
+	}
+}
---
+++ systemUsabilityScale/src/test/java/com/sus/app/systemUsabilityScale/SystemUsabilityScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sus.app.systemUsabilityScale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemUsabilityScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
