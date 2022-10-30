ca12212baf5fa67e6dbd7483e27588307643a8e5
simonhaughom <simon.haughom@gmail.com>
2022-10-30 23:25:19 +0100

    submit working, cleanup needed

--- systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/controllers/ScaleController.java
+++ systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/controllers/ScaleController.java
@@ -5,9 +5,14 @@ import com.sus.app.systemUsabilityScale.repositories.ScaleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,6 +49,7 @@ public class ScaleController {
         }
     }
 
+
 /*    @PostMapping("/scales")
     public ResponseEntity<Scale> createScale(@RequestBody Scale scale) {
         try {
@@ -53,6 +59,29 @@ public class ScaleController {
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }*/
+
+/*    @PostMapping("/saveScale")
+    public ResponseEntity<Scale> saveScale(@ModelAttribute Scale scale,
+                            BindingResult errors, Model model) {
+        model.addAttribute("scale", new Scale());
+        return new ResponseEntity<>(scale, HttpStatus.OK);
+    }*/
+
+/*    @GetMapping("/register")
+    public String showForm(Model model) {
+        Scale scale = new Scale();
+        model.addAttribute("scale", scale);
+
+        List<String> listAgreement = Arrays.asList("1 - strongly disagree", "2", "3", "4", "5 - strongly agree");
+        model.addAttribute("listAgreement", listAgreement);
+        return "scale_form";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("scale") Scale scale) {
+        System.out.println(scale);
+        return "register_success";
     }*/
 
     @PutMapping("/scales/{id}")
--- systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/controllers/WebController.java
+++ systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/controllers/WebController.java
@@ -1,11 +1,16 @@
 package com.sus.app.systemUsabilityScale.controllers;
 
+import com.sus.app.systemUsabilityScale.models.Scale;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -20,4 +25,22 @@ public class WebController {
         model.addAttribute("standardDate", new Date());
         return "dates.html";
     }
+
+    @GetMapping("/register")
+    public String showForm(Model model) {
+        Scale scale = new Scale();
+        model.addAttribute("scale", scale);
+
+        List<String> listAgreement = Arrays.asList("1 - strongly disagree", "2", "3", "4", "5 - strongly agree");
+        model.addAttribute("listAgreement", listAgreement);
+
+        return "scale_form";
+    }
+
+    @PostMapping("/register")
+    public String submitForm(@ModelAttribute("scale") Scale scale) {
+        System.out.println(scale);
+        return "register_success";
+    }
+
 }
