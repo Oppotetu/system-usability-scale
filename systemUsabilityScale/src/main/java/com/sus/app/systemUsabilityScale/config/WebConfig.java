f96fc4146a9e96fc6b2212b1f317c51e55d5f013
simonhaughom <simon.haughom@gmail.com>
2022-10-30 21:12:59 +0100

    frontend evaluation page done

---
+++ systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.sus.app.systemUsabilityScale.config;
+
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+
+    @Bean
+    @Description("Spring Message Resolver")
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+}
--- systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/controllers/WebController.java
+++ systemUsabilityScale/src/main/java/com/sus/app/systemUsabilityScale/controllers/WebController.java
@@ -1,7 +1,11 @@
 package com.sus.app.systemUsabilityScale.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class WebController {
@@ -9,5 +13,11 @@ public class WebController {
     @RequestMapping(value = "/scales")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = "/dates")
+    public String getInfo(Model model) {
+        model.addAttribute("standardDate", new Date());
+        return "dates.html";
     }
 }
